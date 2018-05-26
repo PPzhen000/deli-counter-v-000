790ba53cc0472886090339f826c20954da52ffbc
Ariel Zhen <zhenpingping@live.com>
2018-05-26 21:26:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(array)
+  if array.size == 0 
+    puts "The line is currently empty"
+  else 
+    array.each_with_index do|e, index|
+      "The line is currently:" << " #{index+1}. #{e}"
+    end 
+  end 
+end 
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.size} in line."
+end 
+
+def now_serving(array)
+  if array.size == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array[0]}."
+    array.shift()
+  end 
+end 
