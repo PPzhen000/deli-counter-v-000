bab73c50009b1ab2c6e89741b6584ffb5ed1b5df
Ariel Zhen <zhenpingping@live.com>
2018-05-26 21:30:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(array)
   if array.size == 0
-    result = "The line is currently empty"
+    result = "The line is currently empty."
   else
     array.each_with_index do|e, index|
       result = "The line is currently:" << " #{index+1}. #{e}"
